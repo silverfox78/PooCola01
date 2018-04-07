79111569d99286e71039843aef45bce8de6eeba1
Samuel Barrera <sbarrera@gmail.com>
2018-04-07 11:44:58 -0300

    Cola FIFO

--- src/main/java/Lista.java
+++ src/main/java/Lista.java
@@ -2,6 +2,24 @@ public class Lista {
     private Nodo primero;
 
     public void push(Nodo n){
+        Nodo auxiliar = this.primero;
+        while(!auxiliar.getSiguiente().equals(null)){
+            auxiliar = auxiliar.getSiguiente();
+        }
+        auxiliar.setSiguiente(n);
+    }
 
+    public Nodo pop(){
+        if (this.primero.equals(null)){
+            return null;
+        }
+        Nodo retorno = this.primero;
+        retorno.setSiguiente(null);
+        this.primero = this.primero.getSiguiente();
+        return retorno;
+    }
+
+    public Nodo peek(){
+        return this.primero;
     }
 }
